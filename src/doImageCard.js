8c40ffab92d361d9eeaa2b2ebc0280191e464af2
9Evhen7 <96841920+9Evhen7@users.noreply.github.com>
2022-07-02 23:35:13 +0300

    minDone

---
+++ src/doImageCard.js
@@ -0,0 +1,20 @@
+// import { getImages } from './searchImages'
+import { doImageCard } from './doImageCard';
+
+
+
+
+
+const getImages = async (q) => {
+
+};
+
+const doImageCard = () => {
+    c
+    
+}
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,48 +1,93 @@
+import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-const imgRef = document.querySelector('.gallery');
-const formRef = document.querySelector('#search-form')
+const formRef = document.querySelector('#search-form');
+const gallaryRef = document.querySelector('.gallery')
+const lodeMoreBtnRef = document.querySelector('.load-more');
 
-    const searchParams = new URLSearchParams({
+
+const axios = require('axios').default;
+
+const searchParams = new URLSearchParams({
     key: '28351682-e2b71875895c72fa7531eac7b',
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: 'true',
+    per_page: '40'
 });
+
+let page = 1; 
+let lightbox = new SimpleLightbox('.gallery a', { 
+    captionDelay: 300,
+});
+
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+
+async function getImages(q) {
+    searchParams.append('q', q)
+    searchParams.append('page', page)
+    lodeMoreBtnRef.classList.add('hide')
+    const response = await axios.get(`?${searchParams}`);
+    page += 1;
+
+    if (response.data.hits.length == 0) {
+        Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+    } else { 
+        lodeMoreBtnRef.classList.remove('hide');
+        Notiflix.Notify.success(`Hooray! We found ${response.data.totalHits} images.`)
+    }
+        return response.data.hits;
+
+}
+
+function renderCards(imgs) { 
+    const cardsCode = imgs.map(({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads }) => {
+        
+        const code =
+            `<div class="gallery__item">
+                <a href="${largeImageURL}" class="gallery__link">
+                <img src="${webformatURL}" alt="${tags}" title="${tags}" loading="lazy" class='gallery__image'" />
+                <div class="info">
+                    <p class="info-item"> <b>Likes ${likes}</b> </p>
+                    <p class="info-item"> <b>Views ${views}</b> </p>
+                    <p class="info-item"> <b>Comments ${comments}</b> </p>
+                    <p class="info-item"> <b>Downloads ${downloads}</b> </p>
+                </div>
+                <a/>
+        </div>`;
+        return code;
+
+    }).join('');
+
+    gallaryRef.insertAdjacentHTML('beforeend', cardsCode);
+        lightbox.refresh();
+}
+
+
 
 formRef.addEventListener('submit', (e) => { 
     e.preventDefault();
+    page = 1;
+    gallaryRef.innerHTML = '';
+    getImages(formRef.elements.searchQuery.value)
+        .then(data => { 
+            renderCards(data);
+        });
+})
 
-    searchParams.append('q', `${formRef.elements[0].value}`)
+lodeMoreBtnRef.addEventListener('click', () => {
+    getImages(formRef.elements.searchQuery.value)
+        .then(data => {
+            renderCards(data);
 
-    fetch(`https://pixabay.com/api/?${searchParams}`)
-    .then(response => {
-        return response.json();
-    })
-        .then((images) => {
-            const cardsCode = images.hits.map(({
-                webformatURL,
-                largeImageURL,
-                tags,
-                likes,
-                views,
-                comments,
-                downloads }) => {
-            const code =                    
-            `<div class="gallery__item">
-                <a href="${largeImageURL}" class="gallery__link">
-                    <img src="${webformatURL}" alt="${tags}" loading="lazy" class='gallery__image'" />
-                    
-                    <div class="info">
-                        <p class="info-item"> <b>Likes ${likes}</b> </p>
-                        <p class="info-item"> <b>Views ${views}</b> </p>
-                        <p class="info-item"> <b>Comments ${comments}</b> </p>
-                        <p class="info-item"> <b>Downloads ${downloads}</b> </p>
-                    </div>
-                </a>
-            </div>`;
-                return code;
-            }).join('');
-            console.log(cardsCode);
-            imgRef.innerHTML = cardsCode;
-    });
+        })
 })
